69b9ae3c99c600f20010e78bdf2dae04dde38027
JING CHEN <jxc761@case.edu>
2016-09-05 16:01:35 -0400

    script to run lua

---
+++ script/run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# USAGE: run.sh <JOB_NAME> <LUA_SCIRPT_FILENAME> <MODE> <MEMORY>
+# 
+#
+
+# print usage
+if [ "$#" -lt 2 ]; then
+  echo "Usage: "
+  echo "run.sh.sh <JOB_NAME> <LUA_SCIRPT_FILENAME> [<MODE> [<MEMORY>]]"
+  echo "  <LUA_SCIRPT_FILENAME>: relative path to project_dir/src"
+  echo "  <MODE>  : {cpu}   | gpu"
+  echo "  <MEMORY>: {small} | large"
+  exit
+fi
+
+
+set -o nounset
+set -o errexit
+
+
+JOB_NAME=$1
+STATEMENT=$2
+MODE=${3:-cpu}  # cpu | gpu
+MEMORY=${4:-small} # large | small 
+
+ 
+PROJECT_DIR="/home/jxc761/depth"
+SCRIPT_DIR="${PROJECT_DIR}/script"
+LOG_DIR="${PROJECT_DIR}/log"
+FN_OUT="${LOG_DIR}/${JOB_NAME}.out";
+
+test -d "${LOG_DIR}" || mkdir -p "${LOG_DIR}"
+
+
+
+luarocks list
+
+[[ ${MEMORY} == small ]] &&  mem="--mem=20g" || mem="-mem=40g"
+[[ ${MODE} == cpu ]]     &&  partition="--partition=batch" || partition="-p gpufermi --gres=gpu:1"
+
+cmd=(sbatch --job-name="${JOB_NAME}"  --output="${FN_OUT}"  \
+	 --nodes=1 --cpus-per-task=12 "${mem}" "${partition}"   \ 
+	 --export=STATEMENT="\"${STATEMENT}\""                  \
+	 "${SCRIPT_DIR}/run_lua.slurm") 						\
+
+echo "${cmd[@]}"
+
---
+++ script/run_lua.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+
+echo "BEGIN: $(date +'%D %T')"
+START_TIME=$(date +'%s')
+TIME_STAMP=$(date +'%m-%d-%H-%M')
+
+echo "STATEMENT=${STATEMENT}"
+#
+# main content
+#
+
+# change working directory to src
+project_dir='/home/jxc761/depth'
+src_dir='${project_dir}/src'
+cd "${src_dir}" 
+
+# Load troch7 module
+module load torch
+
+# run torch
+th ${STATEMENT} 
+
+
+STOP_TIME=$(date +'%s')
+echo "DONE: $(date +'%D %T')"
+echo "TIME: $(($STOP_TIME-$START_TIME))"

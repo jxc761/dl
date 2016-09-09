d919edb485454a27fbd40bb4102c5c50ca6ae65b
jxc761 <jxc761@hpc1.(none)>
2016-09-08 23:38:09 -0400

    turn off plotting

--- exp01/run.lua
+++ exp01/run.lua
@@ -21,7 +21,7 @@ local function run()
       nIter = 100000,
       usegpu= true,
       fn_evals_txt   = string.format('%s/process.txt', output),
-      fn_evals_svg   = string.format('%s/process.svg', output),
+      fn_evals_svg   = nil, string.format('%s/process.svg', output),
       fn_performance = string.format('%s/performance.txt', output),
       fn_model       = nil, --string.format('%s/model.dat', output),
       fn_parameters  = string.format('%s/parameters.txt', output)
